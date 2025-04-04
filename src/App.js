ea8ce76adfa051fb1fc29d61e14e53f9678b6c42
Sagar Shankaran <Sagar.Shankaran1@marist.edu>
2025-04-03 22:09:56 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const text = "Welcome to the AI powered Chatbot!";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1 className="welcome-text">
+        {text.split("").map((char, index) => (
+          <span key={index} style={{ animationDelay: `${index * 0.1}s` }}>
+            {char}
+          </span>
+        ))}
+      </h1>
+      <AboutPage></AboutPage>
+      <MyButton />
     </div>
+  );
+}
+function MyButton() {
+  return <button>Submit</button>;
+}
+
+function AboutPage() {
+  return (
+    <>
+      <h2>About Page</h2>
+      <p>
+        Hello
+        <br />
+        How are you?
+      </p>
+    </>
   );
 }
 
